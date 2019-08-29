1513a73b9e58a22a381d59d513ccead88095cd0d
aohanhongzhi <aohanhongzhi@qq.com>
2019-08-29 14:59:47 +0800

    Project的setting.gradle中注册module

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 ## log 
 2019/08/28 : 完成gradle各个模块的创建
+2019/08/29 ：在Project的setting.gradle中注册了module，否则新拉下来的代码的module可能不会成为一个gradle项目，SpringBoot无法运行。更换mvn仓库为阿里云镜像仓库。
--- eureka-server/src/main/java/cn/hxy/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/cn/hxy/eurekaserver/EurekaServerApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-@Enable
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
